b70ca0e45dd1e42bd8591856614f8628e36fc8bf
Jiafeng Liao <jliao@purestorage.com>
2020-03-17 14:24:45 -0700

    Update CSI operator build script.

--- operator-csi-plugin/build.sh
+++ operator-csi-plugin/build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 set -xe
-PSO_CSI_OPERATOR_IMG_TAG=${PSO_CSI_OPERATOR_IMG_TAG:-pso-operator:latest}
+PSO_OPERATOR_IMG_TAG=${PSO_OPERATOR_IMG_TAG:-pso-operator:latest}
 IMG_DIR=$(dirname $0)
 HELM_DIR=${IMG_DIR}/..
 if [ -d "${IMG_DIR}/helm-charts" ]; then rm -rf ${IMG_DIR}/helm-charts; fi
 mkdir -p ${IMG_DIR}/helm-charts
 cp -r ${HELM_DIR}/pure-csi ${IMG_DIR}/helm-charts
 
-docker build -t ${PSO_CSI_OPERATOR_IMG_TAG} ${IMG_DIR}
+docker build -t ${PSO_OPERATOR_IMG_TAG} ${IMG_DIR}
